f6752967bf298445aee5bac2348147c7debda237
jhsu <jkuhsu@gmail.com>
2025-07-11 19:23:24 +0800

    Update the UV tool configuration in pyproject.toml If you have error deploying on Google Drive, uncomment the line in pyproject.toml

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ todo_list : list[TodoItem] = []
 
 app = FastAPI()
 
+# Uncomment the following lines if you want to enable CORS for all origins
+# This is useful if you are accessing the API from a different domain or port.
+# from fastapi.middleware.cors import CORSMiddleware
+ 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
